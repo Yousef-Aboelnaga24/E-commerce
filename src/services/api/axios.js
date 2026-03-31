44dda6fc08e6d87467d987f659696cac55e1be22
Yousef Aboelnaga <aboelnaga0200824@gmail.com>
2026-03-31 09:03:08 +0200

    fix vercel config

--- src/services/api/axios.js
+++ src/services/api/axios.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: 'http://127.0.0.1:8000/api',
   headers: {
-    Accept: 'application/json',
-  },
+    Accept: 'application/json'
+  }
 });
 
 api.interceptors.request.use(config => {
